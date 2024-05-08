ba60fa22f92fe47bb9389589f4ce8d093058b855
DoodMac <48062579+jbird84@users.noreply.github.com>
2024-05-08 11:59:49 -0500

    Initial Commit

---
+++ Sources/JoshKinney/JoshKinney.swift
@@ -0,0 +1,16 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+
+import Foundation
+
+
+public class Add {
+    
+    public init() {}
+    
+    func addTwo(val1: Int, val2: Int) {
+        let added = val1 + val2
+        print(added)
+    }
+}
